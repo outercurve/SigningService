da4c9eaa0486b48b9d0ff08cee8b8f8c56e533b8
Eric Schultz <wwahammy@gmail.com>
2013-05-11 15:44:11 -0700

    Implemented Get-Roles and Remove-Roles

--- OutercurveCmdlets/Commands/GetRolesCmdlet.cs
+++ OutercurveCmdlets/Commands/GetRolesCmdlet.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Management.Automation;
+using ClrPlus.Core.Extensions;
 using ClrPlus.Powershell.Core;
+using Outercurve.ToolsLib.Services;
 
 namespace Outercurve.Cmdlets.Commands
 {
@@ -14,7 +17,21 @@ namespace Outercurve.Cmdlets.Commands
         protected override void ProcessRecord()
         {
             base.ProcessRecord();
-            throw new NotImplementedException();
+            try
+            {
+                var userService = new UserService(Credential.UserName, Credential.Password.ToUnsecureString(),
+                                                  ServiceUrl, MessageHandler, ProgressHandler);
+                userService.GetRoles(Username);
+            }
+            catch (AggregateException ae)
+            {
+                this.WriteErrorsAndThrowOnLast(ae.Flatten().InnerExceptions.Select(LazyCreateError));
+            }
+            catch (Exception e)
+            {
+                ThrowTerminatingError(LazyCreateError(e));
+            }
+             
         }
     }
 }
